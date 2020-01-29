39270dea26fb5ac55ec89ba31946e94af13e20e5
Daniel J Feleppa <dfeleppa@gmail.com>
2020-01-29 19:14:45 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,13 +1,13 @@
 def sort_array_asc(array)
-  
+  array.sort
 end
 
-def sort_array_desc(int)
-  
+def sort_array_desc(in
+  array.sort
 end
 
-def sort_array_char_count
-  
+def sort_array_char_count(array)
+  array.sort
 end
 
 def swap_elements(array)
